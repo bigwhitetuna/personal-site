40b8f36c78c16b4b9eddb442bfb25ae7d81f1ada
Houston <houstoncallaway@gmail.com>
2023-09-08 17:49:44 -0400

    init commit

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,18 +12,17 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>Houston Callaway</h2>
+        <p><a href="mailto:houstoncallaway@gmail.com">houstoncallaway@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Houston. I am a <a href="https://www.lmc.edu/">Lees-McRae College</a> graduate,
+        and am currently a Product Manager at <a href="https://www.axon.com/">Axon</a>. Previously,
+        I was a Product Manager at <a href="https://imubit.com/">Imubit</a>. I have also worked as a project or product manager for Tailored Labs,
+        {' '}<a href="https://www.cruiseplanners.com/">Cruise Planners Corporate Office</a>, and Evil Genius Technologies.
       </p>
       <ul className="actions">
         <li>
--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -5,7 +5,7 @@ const Age = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('1991-06-13T17:55:00');
     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
   };
 
@@ -25,16 +25,21 @@ const data = [
     value: <Age />,
   },
   {
+    key: 'counries-lived',
+    label: 'Countries lived',
+    value: '4',
+  },
+  {
     key: 'countries',
     label: 'Countries visited',
-    value: 53,
+    value: 12,
     link:
-      'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
+      'https://www.google.com/maps/d/u/0/edit?mid=198zBJBd_PrYiGG9wT2Eh5FZAjXD6mCw&ll=0.18383389787321391%2C0&z=2',
   },
   {
     key: 'location',
     label: 'Current city',
-    value: 'New York, NY',
+    value: 'Gastonia, North Carolina',
   },
 ];
 
